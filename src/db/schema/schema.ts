453382db1316c63a5f941c915044111f501231f0
Daniel Borges <dlsbdaniel@gmail.com>
2025-07-27 10:15:03 -0300

    feat: add initial audio transcription using Gemini

--- src/db/schema/schema.ts
+++ src/db/schema/schema.ts
@@ -1,7 +1,9 @@
 import { rooms } from './rooms.ts';
 import { questions } from './questions.ts';
+import { audioChunks } from './audio-chunks.ts';
 
 export const schema = {
   rooms,
   questions,
+  audioChunks,
 };
