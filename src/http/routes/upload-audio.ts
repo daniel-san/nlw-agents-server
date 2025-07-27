be0ce2738ce024d3a55edeec42e1a12fb366d7e4
Daniel Borges <dlsbdaniel@gmail.com>
2025-07-27 10:53:15 -0300

    feat: add code to generate embeddings from audio

--- src/http/routes/upload-audio.ts
+++ src/http/routes/upload-audio.ts
@@ -1,6 +1,8 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import { z } from 'zod';
-import { transcribeAudio } from '../../services/gemini.ts';
+import { generateEmbeddings, transcribeAudio } from '../../services/gemini.ts';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/schema.ts';
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -23,9 +25,28 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
       const audioBuffer = await audio.toBuffer();
       const audioAsBase64 = audioBuffer.toString('base64');
 
-      const transcription = await transcribeAudio(audioAsBase64, audio.mimetype);
+      const transcription = await transcribeAudio(
+        audioAsBase64,
+        audio.mimetype
+      );
+      const embeddings = await generateEmbeddings(transcription);
 
-      return { transcription };
+      const result = await db
+        .insert(schema.audioChunks)
+        .values({
+          roomId,
+          transcription,
+          embeddings,
+        })
+        .returning();
+
+      const chunk = result[0];
+
+      if (!chunk) {
+        throw new Error('Erro ao salvar chunk de áudio');
+      }
+
+      return reply.status(201).send({ chunkId: chunk.id });
     }
   );
 };
